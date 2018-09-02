9b5aae5c8386e040e27dd037171a892bf1de4d9e
dmitrydi <d.dmitrook@gmail.com>
2018-09-02 05:17:09 +0000

    accuracy achieved

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -36,10 +36,10 @@ UKF::UKF() {
   // time_us_ is initialized during first call of ProcessMeasurement()
 
   // Process noise standard deviation longitudinal acceleration in m/s^2
-  std_a_ = 1;
+  std_a_ = 0.3;
 
   // Process noise standard deviation yaw acceleration in rad/s^2
-  std_yawdd_ = 0.1;
+  std_yawdd_ = 1.9;
   
   //DO NOT MODIFY measurement noise values below these are provided by the sensor manufacturer.
   // Laser measurement noise standard deviation position1 in m
